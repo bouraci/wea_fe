c5a88ecbedfc2a5e102ebd5a72e773bc29cfd029
Vojtech Hejsek <hejsekvojtech@tutanota.com>
2024-10-03 09:13:59 +0200

    path aliases, fix error handling and use abstract fetcher

--- app/api/fetchers.ts
+++ app/api/fetchers.ts
@@ -1,12 +1,13 @@
+import { fetcher } from '@utils/fetcher'
+
 export const getGreetingMessage = async (name: string) => {
-    const response = await fetch(
-        `/api/greeting`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(name),
-        }
-    );
+    const response = await fetcher(`/api/greeting`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(name),
+    });
+
     return await response.text();
 };
--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,7 @@ export default function Home() {
     () => getGreetingMessage(debouncedName),
     {
       revalidateOnFocus: false,
+      shouldRetryOnError: false,
     },
   );
 
